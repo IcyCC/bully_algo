2715e709dc1ef72e82224be704b19a44eb616709
suchang <1103232282@qq.com>
2019-06-03 17:31:17 +0800

    智能指针

--- src/handy/event_loop.cpp
+++ src/handy/event_loop.cpp
@@ -4,6 +4,9 @@
 
 #include "event_loop.h"
 #include "poller.h"
+#include <sys/poll.h>
+#include<unistd.h>
+#include <sys/socket.h>
 
 namespace handy {
 
@@ -11,11 +14,21 @@ namespace handy {
         done = true;
     }
 
+    bool Timer::Cancel(){
+        if (done){
+            return false;
+        }
+        base->CancelTimer(id);
+        return true;
+    }
+
     Timer::Timer(const Task &_task, int64_t _at, EventLoop *_base, int64_t _repeat) {
         t = _task;
         at = _at;
         base = _base;
         repeat = _repeat;
+        id = base->getTimerId();
+        running = true;
     }
 
     EventLoop * EventLoop::_self = NULL;
@@ -36,24 +49,21 @@ namespace handy {
         return EventLoop::_self;
     }
 
-    Timer *EventLoop::CreateAtTimeTask(const Task &task, int64_t _at) {
-        timer_id ++;
-        auto t = new Timer(task, _at, this, 0);
+    std::shared_ptr<Timer> EventLoop::CreateAtTimeTask(const Task &task, int64_t _at) {
+        auto t = std::make_shared<Timer>(task, _at, this, 0);
         timers.push(t);
         return t;
     }
 
-    Timer *EventLoop::CreateDelayTask(const Task &task, int64_t time) {
-        timer_id ++;
+    std::shared_ptr<Timer> EventLoop::CreateDelayTask(const Task &task, int64_t time) {
         int64_t at = TimeMilli() + time;
         auto t = CreateAtTimeTask(task, at);
         return t;
     }
 
-    Timer * EventLoop::CreateReaptTask(const Task &task, int64_t time) {
-        timer_id ++;
+    std::shared_ptr<Timer> EventLoop::CreateRepeatTask(const Task &task, int64_t time) {
         int64_t at = TimeMilli() + time;
-        auto t = new Timer(task, at, this, time);
+        auto t = std::make_shared<Timer>(task, at, this, time);
         timers.push(t);
         return t;
     }
@@ -74,10 +84,15 @@ namespace handy {
                 break;
             } else {
                 timers.pop();
+                t->running = false;
+                if (cancel_timer_ids.find(t->id) != cancel_timer_ids.end()) {
+                    continue;
+                }
                 t->t();
 
                 if (t->repeat > 0){
                     t->at = now_time + t->repeat;
+                    t->running = true;
                     timers.push(t);
                 } else {
                     t->setDone(); // 执行
@@ -90,6 +105,44 @@ namespace handy {
         while(true) {
             loopOnce();
         }
+    }
+
+    void EventLoop::CancelTimer(int timer_id){
+        cancel_timer_ids.insert(timer_id);
+    }
+
+
+
+    Channel::Channel(handy::EventLoop *_base, int _fd, int _events) {
+        base = _base;
+        fd = _fd;
+        events = _events;
+    }
+
+    Channel::~Channel(){
+        Close();
+    };
+
+    void Channel::Close() {
+        close(fd);
+    }
+
+    void Channel::EnableRead(bool enable){
+        if (enable){
+            events = events |  POLLIN;
+        } else {
+            events = events & (~POLLIN);
+        }
+        base->poller->UpdateChannel(this);
+    };
+
+    void Channel::EnableWrite(bool enable) {
+        if (enable){
+            events = events |  POLLOUT;
+        } else {
+            events = events & (~POLLOUT);
+        }
+        base->poller->UpdateChannel(this);
     }
 
 }
