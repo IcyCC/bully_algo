dd363beb8396dbf98931b3bc19780189e5ec757f
suchang <1103232282@qq.com>
2019-06-04 23:02:18 +0800

    handle error

--- src/node.cpp
+++ src/node.cpp
@@ -3,3 +3,54 @@
 //
 
 #include "node.h"
+
+namespace bully {
+    Message::Message(const std::string& s){
+
+    };
+
+    void Node::Serve() {
+        ping_timer = loop->CreateRepeatTask([this](){
+            if (nodeState == NodeStateType::FLLOW) {
+                 // 从节点每秒发送一次心跳
+                 this->pingLeader();
+                ping_timout_timer = loop->CreateDelayTask([this](){
+                      this->nodeState = NodeStateType::ELECTING;
+
+                    this->election(); //发送选举消息
+
+                    this->election_timeout_timer = loop->CreateDelayTask([this](){
+                        this->nodeState = LEADER;
+                    }, 1000);
+                },
+                1000);
+            }
+
+
+            }, 1000); // 建立心跳任务
+        loop->RunLoop();
+    }
+
+    void Node::ComOnRead(handy::TcpConn * conn) {
+        auto msg = Message(conn->Read());
+
+        if (this->nodeState == NodeStateType::ELECTING && msg.msg == "Answer"){
+            // 当前节点为选举中 并且收到了节点的响应, 取消选举
+            election_timeout_timer->Cancel();
+        }
+
+        else if (this->nodeState == NodeStateType::FLLOW && msg.msg == "Election") {
+            // 当前节点为选举中 并且收到了选举请求 做出回应
+            conn->Send("Answer");
+        }
+
+        else if (this->nodeState == NodeStateType::LEADER && msg.msg == "Ping") {
+            // 当前节点为leader 收到了ping回复pong
+            conn->Send("Pong");
+        } else if (this->nodeState == NodeStateType::FLLOW && msg.msg == "Pong"){
+            // 当前节点为从节点 收到了pong响应 取消心跳超时的选举
+            this->ping_timout_timer->Cancel();
+        }
+    }
+
+}
--- src/node.h
+++ src/node.h
@@ -5,13 +5,41 @@
 #ifndef BULLY_ALGO_NODE_H
 #define BULLY_ALGO_NODE_H
 
+#include "handy/conn.h"
+#include <string>
+#include <map>
+#include <memory>
+
 namespace bully {
     class Message {
+    public:
+        int  from;
+        int to;
+        std::string msg;
+        Message(const std::string &  s);
 
     };
     class Node {
+    private:
+        handy::EventLoop * loop;
     public:
+        enum  NodeStateType{
+            IDLE,
+            ELECTING, // 选举中
+            FLLOW, //从节点
+            LEADER, // 主节点
+        };
+        NodeStateType nodeState;
+
+        std::shared_ptr<handy::Timer> election_timeout_timer;
+        std::shared_ptr<handy::Timer> ping_timer;
+        std::shared_ptr<handy::Timer> ping_timout_timer;
+
+    public:
+        void ComOnRead(handy::TcpConn * conn);
         void Serve(); // 运行服务
+        void election(){};
+        void pingLeader(){};
     };
 }
 
