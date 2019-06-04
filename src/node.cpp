d4586cc1414689bd49a313177b426a8f79aceec2
zhandong <1468767640@qq.com>
2019-06-05 02:06:16 +0800

    finish logic

--- src/node.cpp
+++ src/node.cpp
@@ -17,71 +17,163 @@ Message::Message(std::string &raw) {
 }
 
 Node::Node(int _id, std::vector<int> &_neighbors) {
+    loop = handy::EventLoop::GetInstance();
     id = _id;
-    neighbors = _neighbors;
+    int max = id;
+    for (auto _neighbor: _neighbors) {
+        Neighbor neighbor;
+        neighbor.id = _neighbor;
+        if (max < _neighbor) {
+            max = _neighbor;
+        }
+        neighbors.push_back(neighbor);
+    }
+    if (id == max) {
+        state = state_leader;
+    } else {
+        state = state_follow;
+    }
+    server = handy::TcpServer("127.0.0.1", id);
+    server.Bind();
+    wait_success = false;
 }
 
-int largest_id(int id, std::vector<int> neighbors) {
+void Node::election() {
+    for (auto neighbor: this->neighbors) {
+        neighbor.neighbor_connection.Send("election " + std::to_string(neighbor.id));
+    }
+    this->state = state_election;
+}
+
+
+void Node::success() {
+    for (auto neighbor: this->neighbors) {
+        neighbor.neighbor_connection.Send("success " + std::to_string(neighbor.id));
+    }
+    this->state = state_leader;
+    this->wait_success = false;
+}
+
+
+int largest_id(int id, std::vector<Neighbor> &neighbors) {
     int max = id;
     for (auto neighbor:neighbors) {
-        if (max < neighbor) {
-            max = neighbor;
+        if (max < neighbor.id) {
+            max = neighbor.id;
         }
     }
     return max;
 }
 
-int get_index(int id, std::vector<int> neighbors) {
+int Node::get_leader() {
     for (int i = 0; i < neighbors.size(); i++) {
-        if (neighbors[i] == id) {
+        if (neighbors[i] == this->leader) {
             return i;
         }
     }
+    this.id;
 }
 
 void Node::Serve() {
 
-    int leader = largest_id(id, neighbors);
+    leader = largest_id(id, neighbors);
 
-    auto loop = handy::EventLoop::GetInstance();
-
-    handy::TcpServer server("127.0.0.1", id);
-
-    std::vector<handy:: & TcpConn> neighbor_connections;
+    std::shared_ptr<handy::Timer> beat_task;
 
     for (auto neighbor:neighbors) {
         handy::TcpConn client(loop, "127.0.0.1", neighbor, 100, "127.0.0.1");
-        neighbor_connections.push_back(client);
+        neighbor.neighbor_connection = client;
     }
 
-    server.OnConnected([](handy::TcpConn *c) {
-        auto resv_data = c->ReadBuffer();
-        Message message(resv_data);
-        if (message.op == "beat") {
-            c->Send("alive");
-        } else if (message.op == "election") {
-            if (id == largest_id(id, neighbors)) {
-                leader = id;
-                for (auto neighbor_connection:neighbor_connections) {
-                    neighbor_connection.Send("success " + std::to_string(id));
-                }
-            } else if (id > atoi(message.params[0])) {
-                c->Send("exist " + std::to_string(id))
-                for (auto neighbor_connection:neighbor_connections) {
-                    neighbor_connection.Send("election " + std::to_string(id));
-                }
-            }
-        } else if (message.op == "success") {
-            leader = atoi(message.params[0]);
+    auto end_beat_func = [this]() {
+        this->beat_timer.Cancel();
+        if (this->id != largest_id(this->id, this->neighbors)) {
+            this->election();
+        } else {
+            this->success();
         }
-    });
-    server.Bind();
-
-    auto beat_func = []() {
-        if (id != leader) {
-            neighbor_connections[get_index(id, neighbor_connections)].Send("beat");
-        }
+        this->wait_success = false;
     };
+
+    auto beat_func = [this]() {
+        this->neighbors[this->get_leader()].neighbor_connection.Send("beat");
+    };
+    auto election_func = [this]() {
+        if (this->id != largest_id(this->id, this->neighbors)) {
+            this->election();
+        } else {
+            this->success();
+        }
+        this->wait_success = false;
+    };
+
+    auto success_func = [this]() {
+        this->success();
+    };
+
+    if (this->state != state_leader) {
+        this->election_timer = loop->CreateDelayTask(election_func, 5000);
+        this->beat_timer = loop->CreateRepeatTask(beat_func, 5000);
+        this->end_beat_timer = loop->CreateDelayTask(end_beat_func, 10000);
+    }
+
+    for (auto neighbor:neighbors) {
+        neighbor.neighbor_connection.OnRead([this, &election_func, &success_func](handy::TcpConn *c) {
+            auto data = c->Read();
+            Message message(data);
+            if (this->state == state_follow && message.op == "beat") {
+                c->Send("alive");
+            } else if (this->state == state_follow && message.op == "election" && atoi(message.params[0]) < this->id) {
+                c->Send("exist " + std::to_string(this->id));
+                this->election();
+                this->success_timer = loop->CreateDelayTask(success_func, 5000);
+            } else if (this->state == state_follow && message.op == "election" && atoi(message.params[0]) > this->id) {
+                if (this->wait_success) {
+                    this->wait_success_timer.Cancel();
+                }
+                this->wait_success_timer = loop->CreateDelayTask(election_func, 5000);
+            } else if (this->state == state_follow && message.op == "exist") {
+                if (this->wait_success) {
+                    this->wait_success_timer.Cancel();
+                }
+                this->wait_success_timer = loop->CreateDelayTask(election_func, 5000);
+            } else if (this->state == state_election && message.op == "election" &&
+                       atoi(message.params[0]) > this->id) {
+                this->state = state_follow;
+                this->success_timer.Cancel();
+                if (this->wait_success) {
+                    this->wait_success_timer.Cancel();
+                }
+                this->wait_success_timer = loop->CreateDelayTask(election_func, 5000);
+            } else if (this->state == state_election && message.op == "election" &&
+                       atoi(message.params[0]) < this->id) {
+                c->Send("exist " + std::to_string(this->id));
+                this->success_timer = loop->CreateDelayTask(success_func, 5000);
+            } else if (this->state == state_election && message.op == "exist") {
+                this->state = state_follow;
+                this->success_timer.Cancel();
+                if (this->wait_success) {
+                    this->wait_success_timer.Cancel();
+                }
+                this->wait_success_timer = loop->CreateDelayTask(election_func, 5000);
+            } else if (message.op == "success") {
+                this->state = state_follow;
+                this->wait_success = false;
+                this->leader = atoi(message.params[0])
+                if (this->wait_success) {
+                    this->wait_success_timer.Cancel();
+                }
+                this->wait_success_timer.Cancel();
+                this->beat_timer = loop->CreateRepeatTask(beat_func, )
+            } else if (message.op == "election" && this->id == largest_id(this->id, this->neighbors)) {
+                this->success();
+            } else if (message.op == "alive" && this->state == state_follow) {
+                this->end_beat_timer.Cancel();
+                this->end_beat_timer = loop->CreateDelayTask(end_beat_func, 5000)
+            }
+        })
+    }
+
     auto beat_task = loop->CreateRepeatTask(beat_func, 1000);
 
     loop->RunLoop();
--- src/node.h
+++ src/node.h
@@ -6,8 +6,13 @@
 #define BULLY_ALGO_NODE_H
 
 #include <vector>
+#include "./handy/handy.h"
 #include <string>
 
+#define state_beat 0
+#define state_leader 1
+#define state_election 2
+#define state_follow 3
 
 class Message {
 public:
@@ -17,15 +22,40 @@ public:
     Message(std::string &raw);
 };
 
+
+struct Neighbor {
+    int id;
+    handy::TcpConn neighbor_connection;
+    int election;
+};
+
 class Node {
+private:
+    handy::EventLoop *loop;
+    handy::TcpServer &server;
 public:
     int id;
     int state;
-    std::vector<int> neighbors;
+    int leader;
+
+    bool wait_success;
+    std::vector<Neighbor> &neighbors;
+
+    std::shared_ptr<handy::Timer> success_timer;
+    std::shared_ptr<handy::Timer> wait_success_timer;
+    std::shared_ptr<handy::Timer> election_timer;
+    std::shared_ptr<handy::Timer> beat_timer;
+    std::shared_ptr<handy::Timer> end_beat_timer;
+
+
 public:
     Node(int _id, std::vector<int> &_neighbors);
 
+    int get_leader();
+
     void election();
+
+    void success();
 
     void Serve(); // 运行服务
 };
